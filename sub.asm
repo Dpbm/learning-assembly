6c85b31e03114739ef78cb2f32a3fe2d9f3607e9
Dpbm <dpbm136@gmail.com>
2022-11-20 22:49:51 -0300

    created sub test

---
+++ sub.asm
@@ -0,0 +1,54 @@
+EXIT equ 1
+STDIN equ 0
+STDOUT equ 1
+READ equ 3
+WRITE equ 4
+
+section .data
+    enterNumberText db 'coloque um número: '
+    enterNumberTextSize equ $ - enterNumberText
+
+    totalText db 'Numero - 2: '
+    totalTextSize equ $ - totalText
+
+section .bss
+    num resb 2
+    result resb 2
+
+section .text
+    global _start
+
+_start:
+    mov eax, WRITE
+    mov ebx, STDOUT
+    mov ecx, enterNumberText
+    mov edx, enterNumberTextSize
+    int 0x80
+
+    mov eax, READ 
+    mov ebx, STDIN
+    mov ecx, num
+    mov edx, 2
+    int 0x80
+
+    mov ebx, [num]
+    sub ebx, 2
+    add byte [result], '0'
+
+    mov [result], ebx
+
+
+    mov eax, WRITE
+    mov ebx, STDOUT
+    mov ecx, totalText
+    mov edx, totalTextSize
+    int 0x80
+
+    mov eax, WRITE
+    mov ebx, STDOUT
+    mov ecx, result
+    mov edx, 2
+    int 0x80
+
+    mov eax, EXIT
+    int 0x80
